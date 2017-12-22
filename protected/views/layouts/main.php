a5d35be234c34c2e1bf1a05680524c6e5e2fe6fd
danmaster <coredan@gmail.com>
2017-12-22 13:48:39 +0000

    CSS Changes

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,7 +33,8 @@
 	  <?php $this->widget('zii.widgets.CBreadcrumbs', array(
 	    'links'=>$this->breadcrumbs,
 	  )); ?><!-- breadcrumbs -->
-	<?php endif?>	
+	<?php endif?>
+
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
